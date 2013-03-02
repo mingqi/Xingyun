db3f31488cdbca253cb596205e5edab4c2251abf
shaomq <shaomq@gmail.com>
2013-03-02 13:40:57 +0800

    ff

---
+++ spy/src/spy/storage.clj
@@ -0,0 +1,11 @@
+(ns spy.storage)
+
+(defn format_path [instance, interval, timepoint]
+  ()
+  )
+
+(defn fetch
+  "fetch TSD from file storage"
+  [instance, interval, timepoint]
+  ()
+  )
---
+++ spy/test/spy/core_test.clj
@@ -0,0 +1,7 @@
+(ns spy.core-test
+  (:use clojure.test
+        spy.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
