5046e1fa43b9bb7c265dee4dc270cb06347d96e3
shaomq <shaomq@gmail.com>
2013-03-05 15:23:09 +0800

    add new ios2

---
+++ ios2/Xinyun2/DishConfirmViewController.h
@@ -0,0 +1,13 @@
+//
+//  DishConfirmViewController.h
+//  Xinyun2
+//
+//  Created by Mingqi Shao on 3/5/13.
+//  Copyright (c) 2013 Mingqi Shao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DishConfirmViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
